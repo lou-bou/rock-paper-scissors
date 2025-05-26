9cbfa4b5412761be024a2d62f6d1d69e58c8cf15
frixc7 <frixc7@gmail.com>
2025-05-26 19:54:16 +0100

    Create the entire game flow
    Write a function that takes human input and computer repeatedly, plays a
    round and tracks score then returns result at the end.

--- script.js
+++ script.js
@@ -34,39 +34,56 @@ function getHumanChoice() {
     return 0
 }
 
-function playRound(choice1, choice2) {
-    let state;
+function playGame() {
+    function playRound(choice1, choice2) {
+        let state;
 
-    switch ([choice1, choice2].join(',')) {
-        case 'rock,paper':
-        case 'paper,scissors':
-        case 'scissors,rock':
-            state = 0
-            break;
-        case 'paper,rock':
-        case 'scissors,paper':
-        case 'rock,scissors':
-            state = 1
-            break;
-        default:
-            state = 2
+        switch ([choice1, choice2].join(',')) {
+            case 'rock,paper':
+            case 'paper,scissors':
+            case 'scissors,rock':
+                state = 0
+                break;
+            case 'paper,rock':
+            case 'scissors,paper':
+            case 'rock,scissors':
+                state = 1
+                break;
+            default:
+                state = 2
+        }
+
+        if (state == 0) {
+            computerScore++;
+            console.log(`You lose! ${choice2} beats ${choice1}.`);
+        } else if (state == 1) {
+            humanScore++;
+            console.log(`You win! ${choice1} beats ${choice2}.`);
+        } else if (state == 2) {
+            console.log(`Tie! ${choice1} vs ${choice2}.`);
+        }
     }
 
-    if (state == 0) {
-        computerScore++;
-        console.log(`You lose! ${choice2} beats ${choice1}.`);
-    } else if (state == 1) {
-        humanScore++;
-        console.log(`You win! ${choice1} beats ${choice2}.`);
-    } else if (state == 2) {
-        console.log(`Tie! ${choice1} vs ${choice2}.`);
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let round = 0; round < 5; round++) {
+        const humanChoice = getHumanChoice();
+        if (humanChoice == 0) {
+            console.log("Invalid choice from player. Round skipped.");
+            continue;
+        }
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You won! You won ${humanScore} out of 5 rounds.`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lose! Computer won ${computerScore} out of 5 rounds.`);
+    } else {
+        console.log(`Tie! Your score ${humanScore} and the computer's score ${computerScore} are equal.`);
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
-
+playGame();
