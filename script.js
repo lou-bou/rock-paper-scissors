644ecf5a72446b657b0ec7e74fd659f20c804166
frixc7 <frixc7@gmail.com>
2025-05-26 19:50:21 +0100

    Create computer choice functionality
    Create a function that randomly chooses rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+function getComputerChoice() {
+    // Returns rock paper or scissors
+    let choice = parseInt(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            choice = "rock"
+            break;
+        case 1:
+            choice = "paper"
+            break;
+        case 2:
+            choice = "scissors"
+            break;
+    }
+    return choice
+}
