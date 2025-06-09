7cb246837a056c98d5a0f8ac7ee2f392144f39af
frixc7 <frixc7@gmail.com>
2025-06-09 11:33:28 +0100

    Create UI for game
    - Buttons for rock paper and scissors
    - Show human and computer score
    - Show current round
    - Display winner of each round
    - Alert winner after 5 rounds
    
    Problem:
    - 5th round winner isn't displayed and alert happends immediately.

--- script.js
+++ script.js
@@ -15,75 +15,125 @@ function getComputerChoice() {
     return choice
 }
 
-function getHumanChoice() {
-    // Get the user to choose rock, paper or scissors
-    let choice = prompt("Enter a valid choice: ");
+function playRound(choice1, choice2) { // winner logic
+    let state;
 
-    if (choice != null) {
-        choice = choice.toLowerCase();
-        switch (choice) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            return choice
+    switch ([choice1, choice2].join(',')) {
+        case 'rock,paper':
+        case 'paper,scissors':
+        case 'scissors,rock':
+            state = 0
+            break;
+        case 'paper,rock':
+        case 'scissors,paper':
+        case 'rock,scissors':
+            state = 1
+            break;
         default:
-            return 0 // 0 is returned if no valid choice is entered
-        }
+            state = 2
     }
 
-    return 0
+    if (state == 0) {
+        result.textContent = `You lose! ${choice2} beats ${choice1}.`;
+    } else if (state == 1) {
+        result.textContent = `You win! ${choice1} beats ${choice2}.`;
+    } else if (state == 2) {
+        result.textContent = `Tie! ${choice1} vs ${choice2}.`;
+    }
 }
 
-function playGame() {
-    function playRound(choice1, choice2) {
-        let state;
+function Scoring(choice1, choice2) {
+    let state;
 
-        switch ([choice1, choice2].join(',')) {
-            case 'rock,paper':
-            case 'paper,scissors':
-            case 'scissors,rock':
-                state = 0
-                break;
-            case 'paper,rock':
-            case 'scissors,paper':
-            case 'rock,scissors':
-                state = 1
-                break;
-            default:
-                state = 2
-        }
+    switch ([choice1, choice2].join(',')) {
+        case 'rock,paper':
+        case 'paper,scissors':
+        case 'scissors,rock':
+            state = 0
+            break;
+        case 'paper,rock':
+        case 'scissors,paper':
+        case 'rock,scissors':
+            state = 1
+            break;
+        default:
+            state = 2
+    }
 
+    return state
+}
+
+function displayScore(human, computer) {
+
+    if (human > computer) {
+        score = `You won! You won ${human} out of 5 rounds.`;
+    } else if (computer > human) {
+        score = `You lose! Computer won ${computer} out of 5 rounds.`;
+    } else {
+        score = `Tie! Your score ${human} and the computer's score ${computer} are equal.`;
+    }
+
+    alert(score);
+}
+
+function reset() {
+    round = 0;
+    humanScore = 0;
+    computerScore = 0
+
+    roundContent.textContent = `Round: ${round}`;
+    humanScoreContent.textContent = `Human: ${humanScore}`;
+    computerScoreContent.textContent = `Computer: ${computerScore}`;
+    result.textContent = "";
+}
+
+// DOM
+const buttons = document.querySelectorAll("button");
+const container = document.querySelector("#results");
+const result = document.createElement("p");
+const humanScoreContent = document.createElement("p");
+const computerScoreContent = document.createElement("p");
+const roundContent = document.createElement("p");
+
+// scoring
+let round = 0;
+let humanScore = 0;
+let computerScore = 0;
+let score;
+humanScoreContent.textContent = `Human: ${humanScore}`;
+computerScoreContent.textContent = `Computer: ${computerScore}`;
+roundContent.textContent = `Round: ${round}`;
+container.appendChild(humanScoreContent);
+container.appendChild(computerScoreContent);
+container.appendChild(roundContent);
+container.appendChild(result);
+
+buttons.forEach((button) => { // PROBLEM: fifth round result isnt shown
+    button.addEventListener("click", () => {
+        let computerChoice = getComputerChoice();
+        let humanChoice = button.id;
+        console.log(humanChoice);
+        console.log(computerChoice);
+        playRound(humanChoice, computerChoice);
+        let state = Scoring(humanChoice, computerChoice);
+        console.log(state);
         if (state == 0) {
             computerScore++;
-            console.log(`You lose! ${choice2} beats ${choice1}.`);
+            computerScoreContent.textContent = `Computer: ${computerScore}`;
+            console.log(`computer ${computerScore}`);
         } else if (state == 1) {
             humanScore++;
-            console.log(`You win! ${choice1} beats ${choice2}.`);
-        } else if (state == 2) {
-            console.log(`Tie! ${choice1} vs ${choice2}.`);
+            humanScoreContent.textContent = `Human: ${humanScore}`;
+            console.log(`human ${humanScore}`);
+        } else {
+            humanScoreContent.textContent = `Human: ${humanScore}`;
         }
-    }
+        round++;
+        roundContent.textContent = `Round: ${round}`;
 
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let round = 0; round < 5; round++) {
-        const humanChoice = getHumanChoice();
-        if (humanChoice == 0) {
-            console.log("Invalid choice from player. Round skipped.");
-            continue;
+        if (round == 5) {
+            displayScore(humanScore, computerScore);
+            reset();
         }
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`You won! You won ${humanScore} out of 5 rounds.`);
-    } else if (humanScore < computerScore) {
-        console.log(`You lose! Computer won ${computerScore} out of 5 rounds.`);
-    } else {
-        console.log(`Tie! Your score ${humanScore} and the computer's score ${computerScore} are equal.`);
-    }
-}
-
-playGame();
+    });
+});
